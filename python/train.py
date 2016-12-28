0b5ec71d5d9f639185073fc88c3b559d4401e18f
Mike Woods <mike@intrinsic.com>
2016-12-28 15:55:14 -0800

    WIP

---
+++ python/train.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import sys
+import pprint
+
+NGRAM_COUNT = 3
+counts = {}
+
+
+def ngrams(word, n):
+    pad = [None for _ in range(n - 1)]
+    x = pad + list(word) + pad
+    return [tuple(chars) for chars in zip(*(x[i:] for i in range(n)))]
+
+
+def read_lines_as_ngrams(from_file, n):
+    with open(from_file, 'r') as f:
+        for line in f:
+            word = line.strip()
+            yield word, ngrams(word, n)
+
+
+for word, ngram_list in read_lines_as_ngrams(sys.argv[1], NGRAM_COUNT):
+    for ngram in ngram_list:
+        if ngram not in counts:
+            counts[ngram] = 0
+        counts[ngram] += 1
+
+
+pprint.pprint(counts)
